8f70262e5906ae6798157826f296829b61112ae9
superavatar888 <superavatar888@gmail.com>
2026-08-10 01:00:55 +0800

    collectKeys API_A_/INFURA_A_

--- sell.js
+++ sell.js
@@ -288,7 +288,6 @@ async function main() {
             expirationTime: expirationTime,
             accountAddress: OWNER_ADDRESS,
             listingTime: listingTime,
-            includeOptionalCreatorFees: false,
         }), 60000, "createListing 超时(60s)");
 
         Logger.success(`Successfully created a listing! tokenId: ${tokenId}, price: ${price} ETH, cost sec = ${(Date.now() / 1000 - current_time).toFixed(2)}, current_index: ${current_index}`);
